9e2fa77e21b555db58f84a56dbd7642a659b1782
Macx <Macx@MacxdeMacBook-Pro-2.local>
2016-08-12 17:43:27 +0800

    first commit

---
+++ JSUsefulDemo/JSUsefulDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSUsefulDemo
+//
+//  Created by Macx on 16/8/10.
+//  Copyright © 2016年 gaojs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
