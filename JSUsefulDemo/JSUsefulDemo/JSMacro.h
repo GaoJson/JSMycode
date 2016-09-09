7c35f4cbca0a9cb2e988ceb3328a207bbd26da66
Macx <Macx@MacxdeMacBook-Pro-2.local>
2016-09-09 10:54:57 +0800

    manr

--- JSUsefulDemo/JSUsefulDemo/JSMacro.h
+++ JSUsefulDemo/JSUsefulDemo/JSMacro.h
@@ -9,7 +9,38 @@
 #ifndef JSMacro_h
 #define JSMacro_h
 
+/** 单例  宏定义 */
+#undef	AS_SINGLETON
+#define AS_SINGLETON(class) \
++ (instancetype)sharedInstance; \
++ (void)distroyInstance;
 
+#undef	DEF_SINGLETON
+#define DEF_SINGLETON(class) \
+static class *sharedInstance##class; \
++ (instancetype)sharedInstance { \
+@synchronized (self) { \
+if (sharedInstance##class == nil) { \
+sharedInstance##class = [[self alloc] init]; \
+} \
+} \
+return sharedInstance##class; \
+} \
++ (id)allocWithZone:(struct _NSZone *)zone {\
+if (sharedInstance##class == nil) {\
+sharedInstance##class = [super allocWithZone:zone];\
+}\
+return sharedInstance##class;\
+}\
+- (id)copyWithZone:(NSZone *)zone\
+{\
+return sharedInstance##class;\
+}\
++ (void)distroyInstance { \
+@synchronized (self) { \
+sharedInstance##class = nil; \
+} \
+}
 
 
 #endif /* JSMacro_h */
