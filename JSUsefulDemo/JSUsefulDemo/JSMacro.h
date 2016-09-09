09c4651ecaf66685103b34dfda0270db3a2e713c
Macx <Macx@MacxdeMacBook-Pro-2.local>
2016-09-09 10:35:59 +0800

    macro

---
+++ JSUsefulDemo/JSUsefulDemo/JSMacro.h
@@ -0,0 +1,15 @@
+//
+//  JSMacro.h
+//  JSUsefulDemo
+//
+//  Created by Macx on 16/9/9.
+//  Copyright © 2016年 gaojs. All rights reserved.
+//
+
+#ifndef JSMacro_h
+#define JSMacro_h
+
+
+
+
+#endif /* JSMacro_h */
